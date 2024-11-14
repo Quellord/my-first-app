0673d2dfafc3fa8743d545df6ee64ccd11336028
Quellord <aman15gupta0705@gmail.com>
2024-11-14 13:07:11 +0530

    Temporarily rename Navbar.jsx to temp.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,14 @@
-import Greetings from "./components/Greetings";
-import UserStatus from "./components/UserStatus";
-import Weather from "./components/Weather"
-// import "./index.css";
 
 // there are three way to add css 
 // 1.using {{}} to make mapp in that same Linter
 // 2.using another map list as style
 // 3.adding new css file and importing it
+// import "./index.css";
 
+import NavBar from "./components/NavBar";
 
-// css in part way
-const secstyle ={
-  backgroundColor:"teal" ,
-  padding:"4rem"
-}
 
 const App = ()=>{
-  return (
-  // <section style={{backgroundColor:"teal"}}>
-    <section style={secstyle}>
-      <Weather/>
-      <UserStatus log ={false} admin={true}/>
-      <Greetings time="afternoon"/>
-  </section>);
+  return <NavBar/>
 };
 export default App;
--- src/components/Greetings.jsx
+++
@@ -1,9 +0,0 @@
-
-const Greetings = ({time}) => {
-  return (
-    //stlye has 2 {{}} remember
-    <h3 style={{color: "red"}}>Good {time}</h3>
-  )
-}
-
-export default Greetings
